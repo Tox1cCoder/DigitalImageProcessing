669000e9b6c185d8fc3fe6287c1e92162f6793d9
Tox1cCoder <hroryu.nguyen@gmail.com>
2023-10-09 12:04:49 +0700

    Init

---
+++ main.py
@@ -0,0 +1,54 @@
+import streamlit as st
+
+
+def load_vdsr_model():
+    pass
+
+
+def superResolution():
+    video_capture = cv2.VideoCapture(video_file)
+
+    while True:
+        ret, frame = video_capture.read()
+        if not ret:
+            break
+
+        # Perform super-resolution using the VDSR model
+        # You need to implement this part using your VDSR model
+        super_res_frame = model.process(frame)  # Replace 'model.process' with your actual VDSR processing code
+
+        # Display the super-resolved frame
+        st.image(super_res_frame, channels="BGR")
+
+    video_capture.release()
+
+
+def main():
+    st.title("Welcome to Video Processing App!")
+
+    st.subheader("""
+        zzz
+    """)
+
+    st.sidebar.title("Select Activity")
+    choice = st.sidebar.selectbox("Menu", (
+        "Super-Resolution", "2", "3"))
+
+    if choice == "Super-Resolution":
+
+        superResolution()
+        st.subheader("Upload a Video for Super-Resolution")
+        uploaded_video = st.file_uploader("Choose a video...", type=["mp4", "avi"])
+
+        if uploaded_video is not None:
+            model = load_vdsr_model()
+            superResolution(uploaded_video, model)
+
+
+    elif choice == "About":
+        st.subheader("About This App")
+        st.write("This app performs super-resolution on uploaded videos using the VDSR model.")
+
+
+if __name__ == '__main__':
+    main()
