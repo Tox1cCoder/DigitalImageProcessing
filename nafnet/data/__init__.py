15c5e0a6d42cc5aecfb72ec44f14501d99ee6b58
Nguyen Duy Thai <92375607+Tox1cCoder@users.noreply.github.com>
2023-12-10 01:28:52 +0700

    Update __init__.py

--- nafnet/data/__init__.py
+++ nafnet/data/__init__.py
@@ -13,9 +13,9 @@ import torch.utils.data
 from functools import partial
 from os import path as osp
 
-from basicsr.data.prefetch_dataloader import PrefetchDataLoader
-from basicsr.utils import get_root_logger, scandir
-from basicsr.utils.dist_util import get_dist_info
+from nafnet.data.prefetch_dataloader import PrefetchDataLoader
+from nafnet.utils import get_root_logger, scandir
+from nafnet.utils.dist_util import get_dist_info
 
 __all__ = ['create_dataset', 'create_dataloader']
 
