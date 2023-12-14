ba31ea086306a85f9a9dca64615399535d6d6c8e
Le Nhat Minh <90740750+nhatminh297@users.noreply.github.com>
2023-12-14 17:57:58 +0700

    add anime super resolution

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 from streamlit_image_comparison import image_comparison
 from app_funcs import *
-
+import torch
+import gc
 
 def Image_Restoration(method):
     upload_path = "uploads/"
@@ -18,10 +19,13 @@ def Image_Restoration(method):
             
             if method == "Low Light":
                 enhance_image(uploaded_image, downloaded_image)
+                torch.cuda.empty_cache()
             elif method == "Blurry":
                 NAFNetBlur(uploaded_image)
+                torch.cuda.empty_cache()
             elif method == "Denoise":
                 NAFNetNoise(uploaded_image)
+                torch.cuda.empty_cache()
 
             final_image = Image.open(downloaded_image)
             print("Opening ", final_image)
@@ -50,40 +54,48 @@ def Super_Resolution(method):
     upload_path = "uploads/"
     download_path = "downloads/"
     uploaded_file = st.file_uploader("Upload Image", type=["png", "jpg", "bmp", "jpeg"], accept_multiple_files=False)
-    
+
     if uploaded_file is not None:
         with open(os.path.join(upload_path, uploaded_file.name), "wb") as f:
             f.write((uploaded_file).getbuffer())
-
-        with st.spinner(f"Enhancing... "):
-            uploaded_image = os.path.abspath(os.path.join(upload_path, uploaded_file.name))
-            downloaded_image = os.path.abspath(os.path.join(download_path, str("enhanced_" + uploaded_file.name)))
-            if method == "BSRGan":
-                # super_resolution(model_path="models\BSRGAN.pth", uploaded_image=uploaded_image)
-                super_resolution(model_path="/content/drive/MyDrive/models/BSRGANx2.pth", uploaded_image=uploaded_image)
-            elif method == "RealESRGAN+":
-                # sr_real_esrgan(model_path="models\RealESRGAN_x2plus.pth", input_path=uploaded_image, scale=2)
-                sr_real_esrgan(model_path="/content/drive/MyDrive/models/RealESRGAN_x4plus.pth", input_path=uploaded_image, scale=4)
             
-            print(downloaded_image)
-            final_image = Image.open(downloaded_image)
-            print("Opening ", final_image)
+        uploaded_image = os.path.abspath(os.path.join(upload_path, uploaded_file.name))
+        downloaded_image = os.path.abspath(os.path.join(download_path, str("enhanced_" + uploaded_file.name)))
+        
+        if method == "BSRGan":
+            types = st.selectbox("Types:", ("Scale x1", "Scale x2"))
+            if st.button("Process"):
+                with st.spinner(f"Enhancing... "):
+                    super_resolution(uploaded_image=uploaded_image, types=types)
+                    torch.cuda.empty_cache()
 
-            image_comparison(
-                img1=uploaded_image,
-                img2=final_image,
-                label1="Original",
-                label2="Enhanced",
-            )
-            file_extension = uploaded_file.name.split(".")[-1]
-            with open(downloaded_image, "rb") as file:
-                if st.download_button(
-                        label="Download Enhanced Image",
-                        data=file,
-                        file_name=str("enhanced_" + uploaded_file.name),
-                        mime=f'image/{file_extension}'
-                ):
-                    download_success()
+        elif method=="RealESRGAN+":
+            types = st.selectbox("Types:", ("Scale x2", "Scale x4", "Anime x4"))
+            face =st.checkbox("Face enhance")
+            if st.button("Process"):
+                with st.spinner(f"Enhancing... "):
+                    sr_real_esrgan(input_path=uploaded_image, scale=4, types=types, face_enhance=face)
+                    torch.cuda.empty_cache()
+
+            # print(downloaded_image)
+            # final_image = Image.open(downloaded_image)
+            # print("Opening ", final_image)
+
+        image_comparison(
+            img1=uploaded_image,
+            img2=downloaded_image,
+            label1="Original",
+            label2="Enhanced",
+        )
+        file_extension = uploaded_file.name.split(".")[-1]
+        with open(downloaded_image, "rb") as file:
+            if st.download_button(
+                    label="Download Enhanced Image",
+                    data=file,
+                    file_name=str("enhanced_" + uploaded_file.name),
+                    mime=f'image/{file_extension}'
+            ):
+                download_success()
 
     else:
         st.warning('⚠ Please upload your Image file')
